45b1368e5897286081b4f1db04ded957ecc177e8
James Vos <almagestfraternite@gmail.com>
2015-10-13 22:16:40 +0200

    add icons

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -9,17 +9,21 @@ App.info({
   website: 'http://example.com'
 });
 
-// Set up resources such as icons and launch screens.
 App.icons({
-  //'iphone': 'icons/icon-60.png',
-  //'iphone_2x': 'icons/icon-60@2x.png',
-  // ... more screen sizes and platforms ...
+  'iphone': 'icons/icon-60.png',
+  'iphone_2x': 'icons/icon-60@2x.png',
+  'ipad': 'icons/icon-72.png',
+  'ipad_2x': 'icons/icon-72@2x.png'
 });
 
 App.launchScreens({
-  //'iphone': 'splash/Default~iphone.png',
-  //'iphone_2x': 'splash/Default@2x~iphone.png',
-  // ... more screen sizes and platforms ...
+  'iphone': 'splash/Default~iphone.png',
+  'iphone_2x': 'splash/Default@2x~iphone.png',
+  'iphone5': 'splash/Default-568h@2x~iphone.png',
+  'ipad_portrait': 'splash/Default-Portrait~ipad.png',
+  'ipad_portrait_2x': 'splash/Default-Portrait@2x~ipad.png',
+  'ipad_landscape': 'splash/Default-Landscape~ipad.png',
+  'ipad_landscape_2x': 'splash/Default-Landscape@2x~ipad.png'
 });
 
 // Set PhoneGap/Cordova preferences
