0b3200a7aeed254cf2d89bab19a7145f8dcc54b2
James Vos <almagestfraternite@gmail.com>
2016-01-27 14:07:45 +0200

    files enhancement

--- app/lib/fields/fileUpload/fileUpload.js
+++ app/lib/fields/fileUpload/fileUpload.js
@@ -22,12 +22,12 @@ if(Meteor.isClient) {
 
   var getFind = function(name, type) {
     if(!Router.current().data() || !Router.current().data().form) return;
-    return Files.find{
+    return Files.find (){
       'metadata.field': this.name,
       'metadata.parentId': Router.current().params._id,
       'metadata.collectionName': Router.current().data().form.collectionName,
       'metadata.type': type
-    };
+    });
   };
 
   Template.afFileUpload.helpers({
