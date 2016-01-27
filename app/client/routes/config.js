5664bd586db15a3cf0e824a366f99177f6b206a2
James Vos <almagestfraternite@gmail.com>
2016-01-27 12:37:04 +0200

    new file:   app/client/routes/config.js 	new file:   app/client/templates/config/edit.html 	new file:   app/client/templates/config/insert.html new file:   app/client/templates/config/list.html new file:   app/lib/collections/config.js

---
+++ app/client/routes/config.js
@@ -0,0 +1,31 @@
+Router.route('config/list', {
+	parent: 'home',
+  name: 'configList',
+  //waitOn: function() {return Meteor.subscribe("config")},
+  data: function() {
+    return {
+      config:Config.find()
+    }
+  },
+	title: 'List Configs',
+  fastRender: true,
+  where: 'client'
+});
+Router.route('config/insert', {
+	parent: 'configList',
+	title: 'Insert Config',
+  name: 'insertConfig',
+  where: 'client'
+});
+Router.route('config/edit/:_id', {
+  name: 'editConfig',
+	parent: 'configList',
+  //waitOn: function() {return Meteor.subscribe("config")},
+  data: function() {
+    var config = Config.findOne({_id:this.params._id});
+    return {config:config};
+  },
+	title: 'Edit Config',
+  fastRender: true,
+  where: 'client'
+});
