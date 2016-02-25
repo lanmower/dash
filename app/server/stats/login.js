1e64b7792a48b170d977fb7048660910020e349b
James Vos <almagestfraternite@gmail.com>
2016-02-25 11:52:37 +0200

    update missing files

--- app/server/stats/login.js
+++ app/server/stats/login.js
@@ -1,11 +1,11 @@
 Hooks.onLoggedIn = function (userId) {
   console.log("log in hook");
-  if(Meteor.user().profile && (!Meteor.user().profile.picture ||
-      Meteor.user().profile.picture.length != 17 ||
-      !Files.findOne({_id:Meteor.user().profile.picture}))) {
-    console.log("Downloading avatar for:",Meteor.userId());
+  //if(Meteor.user().profile && (!Meteor.user().profile.picture ||
+  //    Meteor.user().profile.picture.length != 17 ||
+  //    !Files.findOne({_id:Meteor.user().profile.picture}))) {
+  //  console.log("Downloading avatar for:",Meteor.userId());
     DownloadAvatar(Meteor.user());
-  };
+  //};
   SetEmail(Meteor.user());
   if(
   (Meteor.user().services.google.name && Meteor.user().profile.name != Meteor.user().services.google.name)
