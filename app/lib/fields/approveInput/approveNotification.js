19f240fa42f3db90bf988ae85460f9ecec742237
James Vos <almagestfraternite@gmail.com>
2016-03-17 13:52:08 +0200

    Notice fix for form insert

--- app/lib/fields/approveInput/approveNotification.js
+++ app/lib/fields/approveInput/approveNotification.js
@@ -60,35 +60,43 @@ var notify = function(userId, doc, form, item) {
   var user = Meteor.users.findOne({_id:doc.createdBy});
   var submitter = Meteor.users.findOne({_id:userId});
   var formFields = form.fields.fetch();
-  var userInput;
   _.each(formFields, function(field) {
     if(field.type == "approveInput") {
       if(doc[field.name] == 'Approved') {
         ++min;
-      }
-      if(field.user == userId) {
-        userInput = field;
       }
     }
   });
   console.log("min", min);
   if(min == item.min) {
     console.log('sending notification');
-    var href = Meteor.absoluteUrl()+'form/update/'+form._id+'/'+doc._id;
-    var approveHref = userInput?Meteor.absoluteUrl()+'form/approve/'+form._id+'/'+doc._id+"/"+userInput._id+"/true":null;
-    var rejectHref = userInput?Meteor.absoluteUrl()+'form/approve/'+form._id+'/'+doc._id+"/"+userInput._id+"/false":null;
-    fields = {'submitter' : submitter, 'name' : user.profile.name,'createdAt' : moment(item.createdAt).format('MMMM Do, YYYY'), 'submitterName' : submitter.profile.name, 'email' : user.profile.email, 'submitterEmail' : submitter.profile.email, 'doc' : doc, 'date' : Date(), 'href' : href, 'approveHref' : approveHref, 'rejectHref' : rejectHref};
-    fields = _.extend(fields, doc);
     if(item.email) {
       _.each(item.email, function(to) {
-        if(to != user.profile.email) {
-          Email.send({
-            to: to,
-            from: 'admin@coas.co.za',
-            subject: _.template(item.mailSubject)(fields),
-            text: _.template(item.mailMessage)(fields),
-            html:_.template(item.mailMessageHtml)(fields)
+        var toUser = Meteor.users.findOne({"profile.email":to});
+        if(toUser) {
+          var userInput = null;
+          _.each(formFields, function(field) {
+            if(field.type == "approveInput") {
+              if(field.user == toUser._id) {
+                userInput = field;
+              }
+            }
           });
+          var approveHref = userInput?Meteor.absoluteUrl()+'form/approve/'+form._id+'/'+doc._id+"/"+userInput._id+"/true":null;
+          var rejectHref = userInput?Meteor.absoluteUrl()+'form/approve/'+form._id+'/'+doc._id+"/"+userInput._id+"/false":null;
+
+          var href = Meteor.absoluteUrl()+'form/update/'+form._id+'/'+doc._id;
+          fields = {'submitter' : submitter, 'name' : user.profile.name,'createdAt' : moment(item.createdAt).format('MMMM Do, YYYY'), 'submitterName' : submitter.profile.name, 'email' : user.profile.email, 'submitterEmail' : submitter.profile.email, 'doc' : doc, 'date' : Date(), 'href' : href, 'approveHref' : approveHref, 'rejectHref' : rejectHref};
+          fields = _.extend(fields, doc);
+          if(to != user.profile.email) {
+            Email.send({
+              to: to,
+              from: 'admin@coas.co.za',
+              subject: _.template(item.mailSubject)(fields),
+              text: _.template(item.mailMessage)(fields),
+              html:_.template(item.mailMessageHtml)(fields)
+            });
+          }
         }
       });
     } else {
