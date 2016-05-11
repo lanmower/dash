712ef25b5ea1855fe032d561bab953c9bc23a3af
James Vos <almagestfraternite@gmail.com>
2016-05-11 18:26:49 +0200

    facebook login capabilities

--- app/server/stats/login.js
+++ app/server/stats/login.js
@@ -6,10 +6,10 @@ Hooks.onLoggedIn = function (userId) {
   //  console.log("Downloading avatar for:",Meteor.userId());
   //};
   SetEmail(Meteor.user());
-  if(
-  (Meteor.user().services && Meteor.user().services.google && Meteor.user().services.google.name && Meteor.user().profile.name != Meteor.user().services.google.name)
-  ) {
+  if(Meteor.user().services && Meteor.user().services.google && Meteor.user().services.google.name && Meteor.user().profile.name != Meteor.user().services.google.name) {
     Meteor.users.update({_id:userId },{"$set":{'profile.name': Meteor.user().services.google.name}});
+  } else if(Meteor.user().services && Meteor.user().services.facebook && Meteor.user().services.facebook.name && Meteor.user().profile.name != Meteor.user().services.facebook.name) {
+      Meteor.users.update({_id:userId },{"$set":{'profile.name': Meteor.user().services.facebook.name}});
   } else if(!Meteor.user().profile || !Meteor.user().profile.name) {
     Meteor.users.update({_id:userId },{"$set":{'profile.name': ''}});
   }
