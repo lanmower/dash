95c74176b787229903ad65794d0567a4170d7b47
James Vos <almagestfraternite@gmail.com>
2016-06-23 00:31:37 +0200

    fixes to account methods

---
+++ app/client/config.js
@@ -0,0 +1,12 @@
+Accounts.ui.config({
+  requestPermissions: {
+    google: [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://apps-apis.google.com/a/feeds/emailsettings/2.0/',
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/tasks',
+    'https://mail.google.com/']
+  }
+});
