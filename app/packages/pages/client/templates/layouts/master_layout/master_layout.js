a2e46ee448590ddbc25a8bdbbc5a79f6d53fa4bb
James Vos <almagestfraternite@gmail.com>
2016-07-07 15:35:07 +0200

    spinner improved

--- app/packages/pages/client/templates/layouts/master_layout/master_layout.js
+++ app/packages/pages/client/templates/layouts/master_layout/master_layout.js
@@ -1,4 +1,7 @@
 playlist = new ReactiveVar([]);
+Template.MasterLayout.rendered = function() {
+    $(".spinner").fadeOut(2000);
+}
 Template.MasterLayout.helpers({
   configured: function () {
     return Accounts.loginServicesConfigured();
