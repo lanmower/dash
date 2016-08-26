353174e05217dea847672b839bd71e5d90a6da0e
James Vos <almagestfraternite@gmail.com>
2016-08-26 15:14:51 +0200

    just checking

--- app/packages/diary/client/widgets/diary/diary.js
+++ app/packages/diary/client/widgets/diary/diary.js
@@ -20,11 +20,7 @@ Template.diary.onRendered( function() {
           $(tag).summernote('code', Diaries.findOne({_id:self.tod.get()}, {reactive:false}).diary);
         }
         var ret = Diaries.findOne({user:Meteor.userId(), date:new Date().setHours(0,0,0,0)}, {reactive:false});
-        if(!ret) {
-          var diary = Diaries.insert({user:Meteor.userId(), date:new Date().setHours(0,0,0,0)});
-          self.tod.set(diary);
-        }
-        else {
+        if(ret) {
           self.tod.set(ret._id);
         }
       }
@@ -48,23 +44,13 @@ Template.diary.rendered = function() {
   var tag = this.find('.diary');
   $(tag).summernote({
   callbacks: {
-    /*oninit: function() {
-       var ne = $('div.note-editable');
-       var ned = $('div.note-editor');
-       ned.css('position', 'absolute');
-       ned.css('top', 0);
-       ned.css('right', 0);
-       ned.css('bottom', 0);
-       ned.css('left', 0);
-       ne.css('position', 'absolute');
-       ne.css('top', 50);
-       ne.css('right', 0);
-       ne.css('bottom', 0);
-       ne.css('left', 0);
-    },*/
     onKeyup: _.debounce(function(event, template) {
       console.log('save');
       diary = $(tag).summernote('code');
+      if(!self.tod.get()) {
+        const diary = Diaries.insert({user:Meteor.userId(), date:new Date().setHours(0,0,0,0)});
+        self.tod.set(diary);
+      }
       Diaries.update({_id:self.tod.get()}, {'$set':{"diary":diary.toString()}});
     }, doneTypingInterval)
   }
