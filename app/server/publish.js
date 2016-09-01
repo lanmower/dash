0fcf2400a748a4864d25bcf169902f4010278349
James Vos <almagestfraternite@gmail.com>
2016-09-01 14:20:34 +0200

    user image subscription

--- app/server/publish.js
+++ app/server/publish.js
@@ -2,6 +2,6 @@ Meteor.publish(null, function (test, test2){
   if(Roles.userIsInRole(this.userId, "admin") || Roles.userIsInRole(this.userId, "user-admin"))
     return [
 		Meteor.roles.find({}),
-		Meteor.users.find(this.userId)
+		Files.find(Meteor.user().profile.picture)
 	]
 }); //test
