b865230badd167108e74f9a373353aaf51bea2b7
James Vos <almagestfraternite@gmail.com>
2016-09-01 13:36:15 +0200

    test

--- app/server/publish.js
+++ app/server/publish.js
@@ -1,4 +1,5 @@
-Meteor.publish(null, function (){
+Meteor.publish(null, function (test, test2){
+	console.log(this);
   if(Roles.userIsInRole(this.userId, "admin") || Roles.userIsInRole(this.userId, "user-admin"))
     return Meteor.roles.find({})
 }); //test
