40b09f86df5c451e22f4a2aafcca7e85cab41262
James Vos <almagestfraternite@gmail.com>
2016-09-15 13:04:50 +0200

    signature applications for all admins

--- app/packages/gmail/server/methods.js
+++ app/packages/gmail/server/methods.js
@@ -50,49 +50,54 @@ Meteor.methods({
         var uri = 'gmail/v1/users/'+user.profile.email+"/messages";
         if(query) uri = uri + '?' + toQueryString({q:query})
         var response = GoogleApi.get(uri, {user:user});
-
         gmailSearch.insert({search:response, user:user._id, query:query});
-        _.each(response.messages, function(item) {
-          var id = item.id;
-          var existing = gmail.findOne({_id: id});
-          if(!existing) {
-            var result = GoogleApi.get('gmail/v1/users/'+user.profile.email+"/messages/"+id, {user:user});
-            var doc = {};
+      } catch(error) {
+        if(error.statusCode == 401) {
+            Meteor.call("exchangeRefreshTokenAdmin", adminUser._id);
 
-            for(i in result.payload.headers) {
-              if(result.payload.headers[i].name == "Subject") doc.subject = result.payload.headers[i].value;
-              if(result.payload.headers[i].name == "From") doc.from = result.payload.headers[i].value;
-              if(result.payload.headers[i].name == "To") doc.to = result.payload.headers[i].value;
-            }
-            for(i in result.payload.parts) {
-              if(result.payload.parts[i].mimeType == "multipart/alternative") {
-                for(var j in result.payload.parts[i].parts) {
-                  doc.htmlBody = decodeRawData(result.payload.parts[i].parts[j].body.data);
-                }
-              }
-              if(result.payload.parts[i].mimeType == "text/plain") {
-                doc.plainBody = decodeRawData(result.payload.parts[i].body.data);
-              }
-              if(result.payload.parts[i].mimeType == "text/html") {
-                doc.htmlBody = decodeRawData(result.payload.parts[i].body.data);
-              }
-            }
-            if(result.payload.body.size) {
-              if(result.payload.mimeType == "text/html") doc.htmlBody = decodeRawData(result.payload.body.data);
-              if(result.payload.mimeType == "text/plain") doc.plainBody = decodeRawData(result.payload.body.data);
-            }
-            doc.user = uid;
-            doc._id = item.id;
-            doc.user = uid;
-            doc.query = query;
-            doc.original = result;
-            gmail.insert(doc);
-          }
         }
-      );
-    } catch(error) {
-      console.log(error);
-    }
+        console.log("Error updating messages");
+        console.log(error);
+      }
+
+      _.each(response.messages, function(item) {
+        var id = item.id;
+        var existing = gmail.findOne({_id: id});
+        if(!existing) {
+          var result = GoogleApi.get('gmail/v1/users/'+user.profile.email+"/messages/"+id, {user:user});
+          var doc = {};
+
+          for(i in result.payload.headers) {
+            if(result.payload.headers[i].name == "Subject") doc.subject = result.payload.headers[i].value;
+            if(result.payload.headers[i].name == "From") doc.from = result.payload.headers[i].value;
+            if(result.payload.headers[i].name == "To") doc.to = result.payload.headers[i].value;
+          }
+          for(i in result.payload.parts) {
+            if(result.payload.parts[i].mimeType == "multipart/alternative") {
+              for(var j in result.payload.parts[i].parts) {
+                doc.htmlBody = decodeRawData(result.payload.parts[i].parts[j].body.data);
+              }
+            }
+            if(result.payload.parts[i].mimeType == "text/plain") {
+              doc.plainBody = decodeRawData(result.payload.parts[i].body.data);
+            }
+            if(result.payload.parts[i].mimeType == "text/html") {
+              doc.htmlBody = decodeRawData(result.payload.parts[i].body.data);
+            }
+          }
+          if(result.payload.body.size) {
+            if(result.payload.mimeType == "text/html") doc.htmlBody = decodeRawData(result.payload.body.data);
+            if(result.payload.mimeType == "text/plain") doc.plainBody = decodeRawData(result.payload.body.data);
+          }
+          doc.user = uid;
+          doc._id = item.id;
+          doc.user = uid;
+          doc.query = query;
+          doc.original = result;
+          gmail.insert(doc);
+        }
+      }      );
+
   },
   getMailLabels: function() {
     var result = GoogleApi.get('gmail/v1/users/'+Meteor.user().profile.email+"/labels");
