c29118f91afe31e6e7ba9ddba66dadee899c2dee
James Vos <almagestfraternite@gmail.com>
2016-10-14 14:21:13 +0200

    removals

--- app/packages/import/files.js
+++
@@ -1,6 +0,0 @@
-//FS.debug = true;
-// Write your package code here!
-
-// Variables exported by this module can be imported by other packages and
-// applications. See files-tests.js for an example of importing.
-export const name = 'files';
