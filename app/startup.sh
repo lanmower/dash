905c13d7c18dd475e47da2d8448df1a047a7d192
James Vos <almagestfraternite@gmail.com>
2016-11-28 09:27:06 +0200

    bpn removed

--- app/startup.sh
+++ app/startup.sh
@@ -1,3 +1,3 @@
 export ANDROID_HOME=~/android-sdk-linux
 export ROOT_URL=https://beanscount.co.za 
-meteor run android-device --settings=../settings.json --mobile-server=https://beanscount.co.za
+meteor run android --settings=../settings.json --mobile-server=https://staging.beanscount.co.za
