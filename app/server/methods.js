aaa75ec191b8dd8b1e0bfb9323c697056de072e1
James Vos <almagestfraternite@gmail.com>
2016-11-28 13:43:59 +0200

    moar

--- app/server/methods.js
+++ app/server/methods.js
@@ -1,4 +1,4 @@
 notify = function(from, body, to, uri) {
   bpNotifications.send({title:"From: "+ from, message:body, url:uri}, to);
-  Push.send({title:"From: "+ from,text:body,query:{userId:to}})
+  Push.send({from:from, title:"From: "+ from,text:body,query:{userId:to}})
 }
