86441ff499e1affdc23d24dd51177d8355679645
James Vos <almagestfraternite@gmail.com>
2016-11-28 15:39:31 +0200

    done

--- app/packages/messaging/shared/collections/messages.js
+++ app/packages/messaging/shared/collections/messages.js
@@ -50,9 +50,7 @@ if (Meteor.isServer) {
     if(user) name = user.profile.name;
 
     //notify(name, doc.body, doc.to, "messages/"+userId);
-    _.forEach(doc.to, function(to) {
-      Meteor.call("notify", name, doc.body, to, "messages/"+userId);
-    });
+    Meteor.call("notify", name, doc.body, doc.to, "messages/"+userId);
   });
 }
 
--- app/server/methods.js
+++ app/server/methods.js
@@ -1,12 +1,27 @@
 Meteor.methods({
   // Subscriptions
   notify: function(from, body, to, uri) {
+  	if(Array.isArray(to)) {
+	    _.forEach(doc.to, function(to) {
+	      notify(from, body, to, uri);
+	    });
+    } else {
+      notify(from, body, to, uri);
+    }
+  }
+});	
+
+notify = function(from, body, to, uri) {
 	Push.send({
 		from:from,
 		title:"From:"+from,
 		text:body,
 		query:{userId:to}
 	});
-    bpNotifications.send({title:"From: "+ from,message:body, url:uri}, to);
-  }
-});	
+	bpNotifications.send({title:"From: "+ from,message:body, url:uri}, to);
+}
+
+Push.addListener('message', function (notification) {
+  console.log(JSON.stringify(notification));
+  Dialogs.alert(notification.message, function(){}, notification.payload.title, 'OK');
+});
