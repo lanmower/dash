e7545b0b95ce0837997999a04573fb432ef3a6a2
James Vos <almagestfraternite@gmail.com>
2016-11-28 13:20:25 +0200

    fix

--- app/client/util.js
+++ app/client/util.js
@@ -5,6 +5,6 @@ UI.registerHelper('isCordova', function(){
 });
 
 notify = function(from, body, to, uri) {
-  bpNotifications.send({title:"From: "+ from, message:body, uri:uri}, to);
+  bpNotifications.send({title:"From: "+ from, message:body, url:uri}, to);
   Push.send({title:"From: "+ from,text:body,query:{userId:to}})
 }
