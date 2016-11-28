ff5c142981b36be7b1a6d282659f7e6a4f197b90
James Vos <almagestfraternite@gmail.com>
2016-11-28 13:35:36 +0200

    moar

--- app/client/util.js
+++ app/client/util.js
@@ -4,7 +4,3 @@ UI.registerHelper('isCordova', function(){
   }
 });
 
-notify = function(from, body, to, uri) {
-  bpNotifications.send({title:"From: "+ from, message:body, url:uri}, to);
-  Push.send({title:"From: "+ from,text:body,query:{userId:to}})
-}
